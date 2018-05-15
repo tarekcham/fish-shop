08055d5234ea1e06cc84435149c5e4cfe49b9f36
tarekcham <tarek.alkhatib1@gmail.com>
2018-05-15 21:51:55 +0200

    orders display

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,8 +1,55 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === "available";
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available
+        </li>
+      );
+    }
+    return (
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
   render() {
-    return <div className="order">Order!!!</div>;
+    const orderIds = Object.keys(this.props.order);
+    // console.log(this.props.order);
+     //console.log(orderIds);
+    const total = orderIds.reduce((prevTotal, key) => {
+    	console.log(key);
+    	console.log(prevTotal)
+      const fish = this.props.fishes[key];
+      // console.log(this.props.fishes);
+      // console.log(fish);
+      const count = this.props.order[key];
+      // console.log(this.props.order);
+      // console.log(count);
+      const isAvailable = fish && fish.status === "available";
+      
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
   }
 }
 
